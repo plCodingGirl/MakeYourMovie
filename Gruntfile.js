a38a0d5a5edc8f402ffca0d798ae9c883d81d558
maarchewkowa <k.guzewska@gmail.com>
2021-02-05 19:23:19 +0100

    Fix grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -470,7 +470,7 @@ module.exports = function(grunt) {
         'concat',
         'ngAnnotate',
         'copy:dist',
-        'cdnify',
+        // 'cdnify',
         'cssmin',
         'uglify',
         'usemin',
